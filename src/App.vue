4dfc1406d63c478469a0fc5274d3a6df145882e7
Charlie Thomason <charlie.thomason@uptake.com>
2017-08-29 15:10:24 -0500

    re-added App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <router-link to="/">Main</router-link>
+        <router-link to="/alt">Alt</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    };
+</script>
+
+<style>
+    .message {
+        color:green;
+    }
+</style>
