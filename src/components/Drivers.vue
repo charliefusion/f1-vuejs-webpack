255c323a12f6358f21534466a96561abddbc8f3f
Charlie Thomason <charlie.thomason@uptake.com>
2017-10-03 14:26:52 -0500

    added constructors to drivers table, expanded sorting functionality

--- src/components/Drivers.vue
+++ src/components/Drivers.vue
@@ -14,15 +14,15 @@
         <div v-if="loading">Loading...</div>
         <table class="table table-striped" v-else>
             <thead>
-                <th v-on:click="sortDrivers('familyName')">Name</th>
-                <th>Code</th>
-                <th v-on:click="sortDrivers('nationality')">Nationality</th>
-                <th v-on:click="sortDrivers('points')">Points</th>
+                <th v-on:click="sortDrivers('familyName')" :class="{'active': sort === 'familyName'}">Name <span class="caret" :class="{ 'reversed': reversed }" v-if="sort === 'familyName'"></span></th>
+                <th v-on:click="sortDrivers('Constructors')" :class="{'active': sort === 'Constructors'}">Teams <span class="caret" :class="{ 'reversed': reversed }" v-if="sort === 'Constructors'"></span></th>
+                <th v-on:click="sortDrivers('nationality')" :class="{'active': sort === 'nationality'}">Nationality <span class="caret" :class="{ 'reversed': reversed }" v-if="sort === 'nationality'"></span></th>
+                <th v-on:click="sortDrivers('points')" :class="{'active': sort === 'points'}">Points <span class="caret" :class="{ 'reversed': reversed }" v-if="sort === 'points'"></span></th>
             </thead>
             <tbody>
                 <tr v-for="driver in drivers">
                     <td>{{driver.Driver.givenName + ' ' + driver.Driver.familyName}}</td>
-                    <td>{{driver.Driver.code}}</td>
+                    <td><span v-for="(team, index) in driver.Constructors">{{team.name}}<span v-if="index+1 < driver.Constructors.length">, </span></span></td>
                     <td>{{driver.Driver.nationality}}</td>
                     <td>{{driver.points}}</td>
                 </tr>
@@ -38,7 +38,9 @@
             message: 'Drivers',
             drivers: [],
             year: '2017',
-            loading: true
+            loading: true,
+            sort: 'points',
+            reversed: false
         }),
         methods: {
             getData() {
@@ -47,20 +49,41 @@
                     console.log(response);
                     this.drivers = response.body.MRData.StandingsTable.StandingsLists[0].DriverStandings;
                     this.loading = false;
+                    this.sort = 'points';
+                    this.reversed = false;
                 }, response => {
                     console.log('error', response);
                 });
             },
             sortDrivers(property) {
-                this.drivers.sort((a, b) => {
-                    let first = (property !== 'points') ? a.Driver[property] : parseInt(a[property], 10);
-                    let second = (property !== 'points') ? b.Driver[property] : parseInt(b[property], 10);
-                    if (first < second)
-                        return -1;
-                    if (first > second)
-                        return 1;
-                    return 0;
-                });
+                let first, second;
+                if (this.sort === property) {
+                    this.drivers.reverse();
+                    this.reversed = !this.reversed;
+                } else {
+                    this.sort = property;
+                    this.reversed = false;
+                    this.drivers.sort((a, b) => {
+                        if (property === 'points') {
+                            first = parseInt(a[property], 10);
+                            second = parseInt(b[property], 10);
+                        } else if (property === 'Constructors') {
+                            first = a.Constructors[0].name;
+                            second = b.Constructors[0].name;
+                        } else {
+                            first = a.Driver[property];
+                            second = b.Driver[property];
+                        }
+                        if (first < second)
+                            return -1;
+                        if (first > second)
+                            return 1;
+                        return 0;
+                    });
+                    if (property === 'points') {
+                        this.drivers.reverse();
+                    }
+                }
             }
         },
         created() {
@@ -72,5 +95,11 @@
 <style>
     h3 {
         font-family: sans-serif;
+    }
+    th.active {
+        color:#428bca;
+    }
+    .caret.reversed {
+        transform: rotate(180deg);
     }
 </style>
